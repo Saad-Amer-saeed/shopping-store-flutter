ff502f610aaf7d0153e7f448a9cb0ad2e28800da
saad-Alqattan <saadamer2727@gmail.com>
2024-11-04 19:27:57 +0300

    item v2

--- shoppingstore/lib/screen/store_item_screen.dart
+++ shoppingstore/lib/screen/store_item_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoppingstore/widget/card_resturant_info.dart';
 
 class StoreItemScreen extends StatelessWidget {
   const StoreItemScreen({
@@ -19,68 +20,7 @@ class StoreItemScreen extends StatelessWidget {
             fit: BoxFit.cover,
           ),
           // Card overlay
-          Positioned(
-            top: 180, // Adjust the vertical position of the card
-            left: 16, // Adjust the horizontal position of the card
-            right: 16, // Ensure the card doesn't go off the screen
-            child: Card(
-              shape: RoundedRectangleBorder(
-                side: const BorderSide(
-                  color: Colors.grey, // Card border color
-                  width: 0.2, // Card border width
-                ),
-                borderRadius:
-                    BorderRadius.circular(8.0), // Rounded corners for the card
-              ),
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.grey, // Border color
-                          width: 1.0, // Border width
-                        ),
-                      ),
-                      child: Image.network(
-                        "https://d2yugwrr6or5n1.cloudfront.net/uploads/1516002195.jpg",
-                        width: 80, // Adjust width as needed
-                        height: 70, // Adjust height as needed
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 15,
-                    ),
-                    const Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("Wooden house"),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.star,
-                              color: Color(0xFFFFE234),
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text('3.0')
-                          ],
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
+          const CardResturantInfo(),
         ],
       ),
     );
