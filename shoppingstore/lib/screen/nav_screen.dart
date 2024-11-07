15d4ebf9d5c91230614df6b8d080b7cb13bf3093
saad-Alqattan <saadamer2727@gmail.com>
2024-11-07 10:17:52 +0300

    styling bar

--- shoppingstore/lib/screen/nav_screen.dart
+++ shoppingstore/lib/screen/nav_screen.dart
@@ -15,6 +15,11 @@ class _TabNavigationScreenState extends State<TabNavigationScreen> {
   int _selectedIndex = 0;
   final int _basketItemCount = 3; // Mock basket item count
 
+  // Customizable values
+  double horizontalMargin = 0; // Horizontal margin
+  double borderRadiusValue = 1; // Border radius for bottom navigation bar
+  double borderWidth = 2.0; // Border width for the bottom navigation bar
+
   // List of pages to show for each tab
   final List<Widget> _pages = <Widget>[
     const MerchentsScreen(),
@@ -33,34 +38,55 @@ class _TabNavigationScreenState extends State<TabNavigationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[_selectedIndex], // Display the selected page
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        items: <BottomNavigationBarItem>[
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.symmetric(horizontal: horizontalMargin),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black, // Border color
+            width: 0.6, // Border width
           ),
-          BottomNavigationBarItem(
-            icon: badges.Badge(
-              badgeContent: Text(
-                '$_basketItemCount',
-                style: const TextStyle(color: Colors.white),
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(18), // Top left corner rounded
+            topRight: Radius.circular(18), // Top right corner rounded
+          ),
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(18), // Top left corner rounded
+            topRight: Radius.circular(18), // Top right corner rounded
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.white, // Background color
+            items: <BottomNavigationBarItem>[
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
               ),
-              child: const Icon(Icons.shopping_cart),
-            ),
-            label: 'Basket',
+              BottomNavigationBarItem(
+                icon: badges.Badge(
+                  badgeContent: Text(
+                    '$_basketItemCount',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  child: const Icon(Icons.shopping_cart),
+                ),
+                label: 'Basket',
+              ),
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.login),
+                label: 'Login',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.blue, // Customize selected item color
+            onTap: _onItemTapped, // Handle item tap
           ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.login),
-            label: 'Login',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue, // Customize selected item color
-        onTap: _onItemTapped, // Handle item tap
+        ),
       ),
     );
   }
 }
+
 
 // Home Screen Widget
